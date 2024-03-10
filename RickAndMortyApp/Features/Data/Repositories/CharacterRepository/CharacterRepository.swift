6ea20dea00d8c6ed2e0bd19a2c3fcae8f7cc881d
goncalveshigino <goncalveshigino20@gmail.com>
2024-03-10 18:08:08 +0100

    feat: adicionar o metodo searchCharacter

--- RickAndMortyApp/Features/Data/Repositories/CharacterRepository/CharacterRepository.swift
+++ RickAndMortyApp/Features/Data/Repositories/CharacterRepository/CharacterRepository.swift
@@ -9,24 +9,61 @@ import Foundation
 
 protocol CharacterRepository {
     func getCharacterList(pageNumber: String?) async throws -> CharacterListResponse
+    func searchCharacter(by name: String, and pageNumber: String?) async throws -> CharacterListResponse
 }
 
 
 class DefaultCharacterRepository: CharacterRepository {
     
     private let apiService: ApiService
+    private let cache = DefaultNSCacheStoreDatasource<String, CharacterListResponse>()
     
     init(apiService: ApiService = DefaultApiService()) {
         self.apiService = apiService
     }
     
     func getCharacterList(pageNumber: String?) async throws -> CharacterListResponse {
+        if let cacheResponse = retreive(by: pageNumber ?? "1") {
+            return cacheResponse
+        }
+        
         do {
             let endpoint = RemoteURL.baseUrl + RemoteURL.characterUrl + "\(RemoteURL.pagination)\(pageNumber ?? "1")"
             let response: CharacterListResponse = try await apiService.getDataFromGetRequest(from: endpoint)
+            
+            self.save(with: pageNumber ?? "1", response: response)
             return response
         } catch  {
             throw error
+        }
+    }
+    
+    func searchCharacter(by name: String, and pageNumber: String?) async throws -> CharacterListResponse {
+        do {
+            return try await apiService.getDataFromGetRequest(from: getEndPointForPagination(by: name, and: pageNumber))
+        } catch {
+            throw error
+        }
+    }
+}
+
+extension DefaultCharacterRepository {
+    private func retreive(by pageNumber: String) -> CharacterListResponse? {
+        //cache.retrieve(forKey: pageNumber)
+        cache[pageNumber]
+    }
+    
+    private func save(with pageNumber: String, response: CharacterListResponse) {
+        cache[pageNumber] = response
+    }
+}
+
+extension DefaultCharacterRepository {
+    private func getEndPointForPagination(by name: String, and pageNumber: String?) -> String {
+        if let pageNumber = pageNumber {
+            return RemoteURL.baseUrl + RemoteURL.characterUrl + "\(RemoteURL.name)\(name)" + "\(RemoteURL.searchPagination)\(pageNumber)"
+        } else {
+            return RemoteURL.baseUrl + RemoteURL.characterUrl + "\(RemoteURL.name)\(name)"
         }
     }
 }
--- RickAndMortyAppTests/Features/Data/CharacterRepository/CharacterRepositoryTest.swift
+++ RickAndMortyAppTests/Features/Data/CharacterRepository/CharacterRepositoryTest.swift
@@ -28,9 +28,18 @@ class CharacterRepositoryTest: XCTestCase {
 
 //MARK: - Success Tests
 extension CharacterRepositoryTest {
-    func testSuccessCase_ResponseEntityIsNotNil() async {
+    func testSuccessCase_getCharacterList() async {
         do {
             let response = try await sut?.getCharacterList(pageNumber: nil)
+            XCTAssertTrue(response?.results.first?.id == 21)
+        } catch {
+            XCTFail("Always receive a response and not throw an error")
+        }
+    }
+    
+    func testSuccessCase_SearchCharacter() async {
+        do {
+            let response = try await sut?.searchCharacter(by: "Rick", and: nil)
             XCTAssertTrue(response?.results.first?.id == 21)
         } catch {
             XCTFail("Always receive a response and not throw an error")
@@ -40,7 +49,34 @@ extension CharacterRepositoryTest {
 
 //MARK: - Failure Tests
 extension CharacterRepositoryTest {
-    func testFailureCase_ResponseEntityIsNilReceiveError() {
-        
+    func testFailureCase_getCharacterList() async {
+        do {
+            let _ = try await sutFailure?.getCharacterList(pageNumber: nil)
+            XCTFail("This test should throw an error")
+        } catch {
+            // test passed
+        }
+    }
+    
+    func testFailureCase_getCharacterListParseError() async {
+        let sut: CharacterRepository = DefaultCharacterRepository(apiService: CharacterListFakeApiServiceParseErrorFailure())
+        do {
+           let _ = try await sut.getCharacterList(pageNumber: nil)
+        } catch {
+            if error is AppError {
+                XCTAssertEqual(error.localizedDescription, AppError.parseError.localizedDescription)
+            } else {
+                XCTFail("This test should throw an parse error")
+            }
+        }
+    }
+    
+    func testFailureCase_SearchCharacter() async {
+        do {
+            let _ = try await sutFailure?.searchCharacter(by: "Rick", and: nil)
+            XCTFail("This test should throw an parse error")
+        } catch {
+            // Test passed
+        }
     }
 }
