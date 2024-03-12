06d930dcc55d213c4fd8e9eb89fb87420d46aed1
goncalveshigino <goncalveshigino20@gmail.com>
2024-03-12 13:21:13 +0100

    feat:adicionado router-navigation

---
+++ RickAndMortyApp/Features/Presentation/Home/View/HomePage.swift
@@ -0,0 +1,15 @@
+//
+//  HomePage.swift
+//  RickAndMortyApp
+//
+//  Created by Goncalves Higino on 12/03/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text("Hello, HomePage!")
+    }
+}
+
